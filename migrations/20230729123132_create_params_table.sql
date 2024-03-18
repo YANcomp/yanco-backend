ef187a7504ef9b50579b949e895860a1e1e60ee6
YANcomp <yanco.db@yandex.ru>
2024-03-18 11:17:26 +0300

    ver 0.1.18.03.1117

---
+++ migrations/20230729123132_create_params_table.sql
@@ -0,0 +1,47 @@
+-- +goose Up
+BEGIN;
+
+SET
+    statement_timeout = 0;
+SET
+    client_encoding = 'UTF8';
+SET
+    standard_conforming_strings = ON;
+SET
+    check_function_bodies = FALSE;
+SET
+    client_min_messages = WARNING;
+SET
+    search_path = public, extensions;
+SET
+    default_tablespace = '';
+SET
+    default_with_oids = FALSE;
+
+-- EXTENSIONS --
+CREATE
+    EXTENSION IF NOT EXISTS pgcrypto;
+CREATE
+    EXTENSION IF NOT EXISTS citext;
+
+-- DROP TABLE --
+DROP TABLE IF EXISTS params CASCADE;
+
+create table params
+(
+    id          serial PRIMARY KEY,
+    header      VARCHAR(250) NOT NULL CHECK ( header <> '' ),
+    description text                     DEFAULT '',
+    targettype  text                     DEFAULT '',
+    target      text                     DEFAULT '',
+    image       text                     DEFAULT '',
+    imagemobile text                     DEFAULT '',
+    iscatalog   boolean                  DEFAULT false,
+    created_at  TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at  TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+COMMIT;
+
+-- +goose Down
+drop table params;
